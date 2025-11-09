0c4d3fceaa5ce4da1e316a2ce07c5963c7905538
Shriram Vibhute <shriram.vibhute08@gmail.com>
2025-11-09 12:26:43 +0530

    Completed the setup of project using cookiecutter data science template.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classifying the sentement of IMDB comments.',
+    author='Shriram Vibhute',
+    license='MIT',
+)
